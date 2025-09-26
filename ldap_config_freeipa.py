6b5ed460be6f1aae5c15f6d54bd2bc526c785074
Thiago Jack de Oliveira <thiagoojack@gmail.com>
2025-09-26 17:46:20 -0300

    fix: duplicate key value Add setting `AUTH_LDAP_USER_QUERY_FIELD = "username"` to prevent duplicated user message error. Closes #5

--- ldap_config_freeipa.py
+++ ldap_config_freeipa.py
@@ -65,6 +65,8 @@ AUTH_LDAP_USER_ATTR_MAP = {
     "last_name": "sn",
 }
 
+AUTH_LDAP_USER_QUERY_FIELD = "username"
+
 '''
 This search ought to return all groups to which the user belongs.
 django_auth_ldap uses this to determine group hierarchy. The string `netbox*` 
